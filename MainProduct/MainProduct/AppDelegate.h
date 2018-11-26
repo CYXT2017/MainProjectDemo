36ae93f242a7627f37bac22d92a2e6b0168b5e51
陈涛 <1223903860@qqcom>
2018-11-26 11:25:05 +0800

    1.主工程初始化

---
+++ MainProduct/MainProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProduct
+//
+//  Created by 陈涛 on 2018/11/26.
+//  Copyright © 2018年 陈涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProduct/MainProduct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProduct
+//
+//  Created by 陈涛 on 2018/11/26.
+//  Copyright © 2018年 陈涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
